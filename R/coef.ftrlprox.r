48db2711acf23fd54c26d0bd3a3184ad08837829
Vilhelm von Ehrenheim <vilhelm.ehrenheim@klarna.com>
2016-05-23 16:11:15 +0200

    Added coef function.

---
+++ R/coef.ftrlprox.r
@@ -0,0 +1,17 @@
+##------------------------------------------------------------------------------
+#' coef.ftrlprox
+#' 
+#' Extract model coefficients.
+#' 
+#' This function can be used to extract the coefficients of a model trained using the ftrlprox package.
+#' 
+#' @param object The model object
+#' @return an array with the regression coefficients
+#' 
+#' @author Vilhelm von Ehrenheim
+#' @export
+##------------------------------------------------------------------------------
+coef.ftrlprox <- function(object) {
+  object$theta
+}
+
