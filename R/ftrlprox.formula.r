dfb6c6ad9e331930e764ac3374b908e57274b211
Vilhelm von Ehrenheim <vilhelm.ehrenheim@klarna.com>
2016-05-27 11:10:02 +0200

    Made formula function call the matrix one.

--- R/ftrlprox.formula.r
+++ R/ftrlprox.formula.r
@@ -31,36 +31,10 @@
 ftrlprox.formula <- function(formula, data, lambda1, lambda2, alpha, beta=1, num_epochs=1, loss=F) {
 
   X <- model.matrix(formula, data)
-  response <- all.vars(formula[[2]])
+  y <- data[[all.vars(formula[[2]])]]
 
-  if (!is.factor(data[[response]]))
-    stop("Dependent variable must be a factor")
-
-  if (nlevels(data[[response]]) != 2)
-    stop("Dependent variable must be a factor with 2 levels")
-
-  y <- as.numeric(data[[response]]) - 1  # Make factor into numeric 0 and 1
-
-  out <- .C("lognet_ftrlprox",
-            X=as.double(X),
-            theta=double(ncol(X)),
-            y=as.double(y),
-            m=as.integer(nrow(X)),
-            n=as.integer(ncol(X)),
-            J=numeric(num_epochs),
-            num_epochs=as.integer(num_epochs),
-            alpha=as.double(alpha),
-            beta=as.double(beta),
-            lambda1=as.double(lambda1),
-            lambda2=as.double(lambda2),
-            loss=as.integer(loss))
-
-  names(out$theta) <- colnames(X)
-
-  out$fx <- formula
-  out$levels <- levels(data[[response]])
-  class(out) <- "ftrlprox"
-
-  out
+  ftrlprox.matrix(X,y,alpha=alpha,beta=beta,
+                  lambda1=lambda1, lambda2=lambda2,
+                  num_epochs=num_epochs, loss=loss)
 }
 
