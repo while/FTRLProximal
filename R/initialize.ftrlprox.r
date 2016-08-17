6fd108d8342f669976acfb50742e64eec6e01200
Vilhelm von Ehrenheim <vilhelm.ehrenheim@klarna.com>
2016-08-17 22:58:19 +0200

    Added support for initializing an empty model.

---
+++ R/initialize.ftrlprox.r
@@ -0,0 +1,45 @@
+##------------------------------------------------------------------------------
+#' Initialize empty FTRL Proximal class
+#' 
+#' Online elastic net regression using the FTRL Proximal algorithm for training.
+#'
+#' This method is intended for setting up a ftrlprox model object before training it using update. 
+#'
+#' @param theta named numeric containing initial coefficients
+#' @param levels character vector containing class labels of target label
+#' @param lambda1 L1 regularization term
+#' @param lambda2 L2 regularization term
+#' @param alpha learning rate parameter
+#' @param beta learning rate parameter controlling decay, defaults to 1.
+#' @param save_loss is to save the loss function during training.
+#' @param ... additional args
+#' @return ftrlprox model object
+#' @author Vilhelm von Ehrenheim
+#'
+#' @method ftrlprox default
+#' @useDynLib FTRLProximal
+#' @export
+##------------------------------------------------------------------------------
+initialize.ftrlprox <- function(theta, levels, lambda1, lambda2, alpha, beta=1, save_loss=F, ...) {
+
+  n <- length(theta)
+
+  # Set up an empty object
+  out <- list(theta = theta,
+              m = 0,
+              n = n,
+              z = numeric(n),
+              nn = numeric(n),
+              J = numeric(0),
+              num_epochs = 1,
+              alpha = alpha,
+              beta = beta,
+              lambda1 = lambda1,
+              lambda2 = lambda2,
+              loss = integer(1),
+              levels = levels)
+
+  class(out) <- "ftrlprox"
+  out
+}
+
---
+++ tests/testthat/test_initialize_ftrlprox.r
@@ -0,0 +1,43 @@
+library(mlbench)
+context("Test initialize and update ftrlprox")
+
+set.seed(1)
+p <- mlbench.2dnormals(100,2)
+
+dat <- data.frame(p$x)
+colnames(dat) <- c("A", "B")
+dat$y <- factor(p$classes, labels=c("G", "B"))
+
+X <- model.matrix(y ~ ., dat)
+
+theta <- numeric(3)
+names(theta) <- c("(Intercept)", "A", "B")
+
+# Set up enpty object
+mdl <- initialize.ftrlprox(theta, c("G", "B"), alpha=1, beta=1, lambda1=0, lambda2=0)
+
+# Update model using the rest of the data this should generate the same result
+# as training once using all data.
+mdl <- update(mdl, X, dat$y)
+
+test_that("Class is ftrlprox", {
+          expect_is(mdl, "ftrlprox")
+})
+
+test_that("Parameter values", {
+          coefs <- mdl$theta
+          names(coefs) <- NULL
+
+          expect_equal(coefs[1], -0.38188318762577)
+          expect_equal(coefs[2], -2.23889313859288)
+          expect_equal(coefs[3], -1.69555552563667)
+})
+
+test_that("Parameter names", {
+          expect_equal(names(mdl$theta), c("(Intercept)", "A", "B"))
+})
+
+test_that("Target levels", {
+          expect_equal(mdl$levels, c("G", "B"))
+})
+
