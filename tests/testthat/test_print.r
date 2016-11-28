9e5df0477aa96d47a65d455b546527631deed42e
Vilhelm von Ehrenheim <vilhelm.ehrenheim@klarna.com>
2016-11-28 23:00:48 +0100

    Added tests for printing of model coefficients.

---
+++ tests/testthat/test_print.r
@@ -0,0 +1,30 @@
+library(mlbench)
+context("Test ftrlprox print")
+
+set.seed(1)
+p <- mlbench.2dnormals(1000,2)
+dat <- as.data.frame(p)
+dat$x.3 <- rnorm(100)
+
+mdl <- ftrlprox(classes ~ ., dat, a=0.3, lambda = 1, alpha = 1)
+
+test_that("Default print", {
+
+          # Test default amount of digits
+          expect_output(print(mdl),
+                        "Coefficients:\\s+\\(Intercept\\)\\s+-0.00677584\\s+x\\.1\\s+-1.75985861\\s+x\\.2\\s+-1.64894623\\s+x\\.3\\s+\\.")
+
+})
+
+
+test_that("Print 3 digits", {
+          # Test 3 digits
+          expect_output(print(mdl, digits=3),
+                        "Coefficients:\\s+\\(Intercept\\)\\s+-0.00678\\s+x\\.1\\s+-1.75986\\s+x\\.2\\s+-1.64895\\s+x\\.3\\s+\\.")
+})
+
+test_that("Print 3 digits and ", {
+          # Test 3 digits
+          expect_output(print(mdl, digits=3, zero.print="z"),
+                        "Coefficients:\\s+\\(Intercept\\)\\s+-0.00678\\s+x\\.1\\s+-1.75986\\s+x\\.2\\s+-1.64895\\s+x\\.3\\s+z")
+})
